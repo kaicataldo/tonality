96e6cc0c588576b2e810448cff3502cedc62c860
Kai Cataldo <kaicataldo@gmail.com>
2014-10-27 20:40:09 -0400

    Set up initial grid

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+//ready function - this script won't load until the dom has been loaded
+$(function() {
+
+  //toggles selected class
+  $('.box').click(function() {
+  $(this).toggleClass("selected");
+  console.log("clicked");
+  });
+
+  for (i = 1; i <= 16; i++) {
+    
+  }
+
+
+});
