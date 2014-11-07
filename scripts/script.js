ff76fedee5f0e53d4a7d747133dad846a5eca68d
Kai Cataldo <kaicataldo@gmail.com>
2014-11-07 03:57:04 -0500

    Settings can now be changed on the fly

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 $(function() {
-
   var settings = {
     soundPack:[],
     fadeInVal:100,
@@ -7,6 +6,7 @@ $(function() {
     loopStatus:null,
     currentCol:1,
     colInterval:120,
+    measures: 1,
     beats:4,
     subdivision:4,
     defaulCols: 16,
@@ -14,6 +14,7 @@ $(function() {
     selectedBlocks: []
   };
 
+//Setup
   buildGrid();
   createSoundPack();
 
@@ -22,21 +23,22 @@ $(function() {
     'min': 1,
     'max': 240,
     'width': 100,
-    'release': getOptions()
+    'release': function (v) { setTempo('colInterval', v)}
   });
 
+//Event Handlers
   //Toggles selected class
   $('body').on('click', '.box', function() {
     $(this).toggleClass("selected");
   });
 
   //Event listeners for toggling Start/Stop
-  //Click button
+    //Click button
    $(".toggle").click(function() {
      toggleClicked();
    });
 
-  //Press spacebar
+    //Press spacebar
   $(window).keypress(function(e) {
     if (e.keyCode == 0 || e.keyCode == 32) {
       if ("activeElement" in document) {
@@ -50,10 +52,23 @@ $(function() {
   $(".clear").click(function() {
     $(".box").removeClass("selected");
   });
-  
-  //Rebuild grid button
-  $(".save").click(function() {
-    getOptions();
+    
+  //Measures/Beats/Subdivision listeners
+  $('.beats-container input').on('change', function() {
+    settings.beats = $('input[name="beats"]:checked', '.beats-container').val(); 
+    resetGrid();
+    $(".box").removeClass("active");
+  });
+
+  $('.subdivision-container input').on('change', function() {
+    settings.subdivision = $('input[name="subdivision"]:checked', '.subdivision-container').val(); 
+    resetGrid();
+    $(".box").removeClass("active");
+  });
+
+  $('.measures-container input').on('change', function() {
+    settings.measures = $('input[name="measures"]:checked', '.measures-container').val(); 
+    console.log
     resetGrid();
     $(".box").removeClass("active");
   });
@@ -71,9 +86,48 @@ $(function() {
     }
   }
 
+//Functions
+ //Dynamically build HTML grid
+  function buildGrid() {
+    var container = $(".grid");
+    settings.totalCols = settings.measures * settings.beats * settings.subdivision;
+    //Loop through each row.
+    for (var colIndex = 1; colIndex <= settings.totalCols; colIndex++) {
+        //Create the skeleton for the row.
+        var col = $("<div>", {
+            "class": "col col-"+ colIndex,
+            "id": "col-"+ colIndex
+        });
+        //Loop through each column
+        for (var rowIndex = 1; rowIndex <= 16; rowIndex++) {
+          //Create skeleton for the column.
+          var row = $("<div>", {
+              "class": "box row-"+rowIndex,
+              "id": "col"+colIndex+"row"+rowIndex,
+              "note": rowIndex
+          });
+          //Append to the row div.
+          col.append(row);
+        }
+        //Finally append this row to the container.
+        container.append(col);
+    }
+  }
+
+  //Dynamically create sound file references
+  function createSoundPack() {
+    for(var soundIndex = 1; soundIndex<=16; soundIndex++) {
+      settings.soundPack[soundIndex-1] = new Howl({
+        urls: ['media/'+soundIndex+'.ogg', 'media/'+soundIndex+'.mp3'],
+        fadeIn: settings.fadeInVal,
+        fadeOut: settings.fadeOutVal
+      });
+    }
+  }
+
   function resetGrid() {
     var container = $(".grid");
-    var newTotalCols = settings.beats * settings.subdivision;
+    var newTotalCols = settings.measures * settings.beats * settings.subdivision;
     if (newTotalCols !== settings.totalCols) {
       if (newTotalCols > settings.totalCols) {
         for (var currentCol = settings.totalCols + 1; currentCol <= newTotalCols; currentCol++) {
@@ -108,68 +162,17 @@ $(function() {
       settings.currentCol = 1;
     }
 
-  //Dynamically build HTML grid
-  function buildGrid() {
-    var container = $(".grid");
-    settings.totalCols = settings.beats * settings.subdivision;
-    //Loop through each row.
-    for (var colIndex = 1; colIndex <= settings.totalCols; colIndex++) {
-        //Create the skeleton for the row.
-        var col = $("<div>", {
-            "class": "col col-"+ colIndex,
-            "id": "col-"+ colIndex
-        });
-        //Loop through each column
-        for (var rowIndex = 1; rowIndex <= 16; rowIndex++) {
-          //Create skeleton for the column.
-          var row = $("<div>", {
-              "class": "box row-"+rowIndex,
-              "id": "col"+colIndex+"row"+rowIndex,
-              "note": rowIndex
-          });
-          //Append to the row div.
-          col.append(row);
-        }
-        //Finally append this row to the container.
-        container.append(col);
-    }
-  }
-
-  function saveSelected() {
-    console.log("");
-  }
-
-  //Dynamically create sound file references
-  function createSoundPack() {
-    for(var soundIndex = 1; soundIndex<=16; soundIndex++) {
-      settings.soundPack[soundIndex-1] = new Howl({
-        urls: ['media/'+soundIndex+'.ogg', 'media/'+soundIndex+'.mp3'],
-        fadeIn: settings.fadeInVal,
-        fadeOut: settings.fadeOutVal
-      });
-    }
-  }
-
-  //Gets the values from each HTML <input> and assigns to the appropriate setting
-  function getOptions() {
-    $(".option").each( function(el) {
-      var dataName = $(this).attr("data-option");
-      var dataVal = $(this).val();
-      //console.log($(this).val());
-      if (dataName === "colInterval") {
-        var milliseconds = Math.round((((60/dataVal)*1000)*100000)/100000)/settings.subdivision;
+  //Set tempo
+  function setTempo(dataName, dataVal) {
+    var milliseconds = Math.round((((60/dataVal)*1000)*100000)/100000)/settings.subdivision;
         //console.log("Sixteenth is precisely "+sixteenth+" milliseconds");
         settings[dataName] = milliseconds;
-      } else {
-        settings[dataName] = dataVal;
-      }
-    });
   }
 
   //Loop through every column
   function gridLoop() {
     if (settings.loopStatus) {
-      settings.totalCols = settings.beats * settings.subdivision;
+      settings.totalCols = settings.measures * settings.beats * settings.subdivision;
       setTimeout( function() {
         $(".col-" + settings.currentCol).children().toggleClass("active");
         if (settings.currentCol == 1) {
