1ade26a8b486341bb8b9b8d0ca43198b71ffaa2f
bsantero <brian.santero@gmail.com>
2014-11-06 18:42:55 -0600

    Merge branch 'Audio-customizations'
    Conflicts:
    	css/style.css
    	index.html
    	media/1.ogg
    	media/10.ogg
    	media/11.ogg
    	media/12.ogg
    	media/13.ogg
    	media/14.ogg
    	media/15.ogg
    	media/16.ogg
    	media/2.ogg
    	media/3.ogg
    	media/4.ogg
    	media/5.ogg
    	media/6.ogg
    	media/7.ogg
    	media/8.ogg
    	media/9.ogg
    	scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 //ready function - this script won't load until the dom has been loaded
 $(function() {
+<<<<<<< HEAD
   var sound1 = new Howl({
     urls: ['media/1.ogg']
   });
@@ -48,9 +49,20 @@ $(function() {
   var sound16 = new Howl({
     urls: ['media/16.ogg']
   });
+=======
+>>>>>>> Audio-customizations
 
-  var i = 1;
-  var keepLoop;
+  var settings = {
+    soundPack:[],
+    fadeInVal:100,
+    fadeOutVal:null,
+    loopStatus:null,
+    currentCol:1,
+    colInterval:120
+  };
+
+  buildGrid();
+  createSoundPack();
 
   //toggles selected class
   $('.box').click(function() {
@@ -65,59 +77,120 @@ $(function() {
     $('.box').removeClass('selected');
   });
 
-  //Play
+  function setInterval(value){
+    console.log("setting interval");
+  }
+
+  //Dynamically build HTML grid
+  function buildGrid() {
+    var container = $(".grid");
+    //Loop through each row.
+    for (var colIndex = 1; colIndex <= 16; colIndex++) {
+        //Create the skeleton for the row.
+        var col = $("<div>", {
+            "class": "col col-"+ colIndex,
+            "id": "col-"+ colIndex
+        });
+        //Loop through each column
+        for (var rowIndex = 1; rowIndex <= 16; rowIndex++) {
+            //Create skeleton for the column.
+            var row = $("<div>", {
+                "class": "box row-"+rowIndex,
+                "id": "col"+colIndex+"row"+rowIndex,
+                "note": rowIndex
+            });
+            //Append to the row div.
+            col.append(row);
+        }
+        //Finally append this row to the container.
+        container.append(col);
+    }
+  }
+
+  //Dynamically create sound file references
+  function createSoundPack (){
+    for(var soundIndex = 1; soundIndex<=16; soundIndex++) {
+      settings.soundPack[soundIndex-1] = new Howl({
+        urls: ['media/'+soundIndex+'.ogg', 'media/'+soundIndex+'.mp3'],
+        fadeIn: settings.fadeInVal,
+        fadeOut: settings.fadeOutVal
+      });
+    }
+  }
+
+
+  function getOptions(){
+    var optionsBox = $.map($('.option'), function(el) {
+      return {name: $(el).attr("data-option"), value: $(el).val()}
+    });
+    for (var optionsIndex = 0; optionsIndex <= optionsBox.length; optionsIndex++){
+      console.log(optionsBox[optionsIndex]); 
+    }
+  }
+
+  //Start button <-> Stop Button 
   function toggleClicked() {
     if ($('.toggle').html() == 'Start') {
       $('.toggle').html('Stop');
-      keepLoop = true;
+      getOptions();
+      settings.loopStatus = true;
       gridLoop();
     }
     else if ($('.toggle').html() == 'Stop') {
       $('.toggle').html('Start');
-      keepLoop = false;
+      settings.loopStatus = false;
     }
+    //console.log($(input[i]).attr("data-name"));
   }
 
+
+  //Loop through every column
   function gridLoop() {
-    if (keepLoop) {
-      setTimeout(function() {
-        var column = ".col-" + i;
-        $(column).children().toggleClass('active');
-        if (i == 1) {
+    if (settings.loopStatus) {
+      setTimeout( function() {
+        $(".col-" + settings.currentCol).children().toggleClass("active");
+        if (settings.currentCol == 1) {
           $(".col-16").children().removeClass("active");
         }
         else {
-          $(".col-" + (i - 1)).children().toggleClass("active");
+          $(".col-"+(settings.currentCol - 1)).children().removeClass("active");
         }
-        playSound(column);
-        i++;
-        if (i <= 16){
+        playSound(".col-" + settings.currentCol);
+        settings.currentCol++;
+        if (settings.currentCol <= 16){
           gridLoop();        
         }
-        else if (i == 17) {
-          i = 1;
+        else if (settings.currentCol == 17) {
+          settings.currentCol = 1;
           gridLoop();
         }
-       }, 120);
+       }, settings.colInterval);
     }
     else {
-      i = 1;
+      settings.currentCol = 1;
       $(".box").removeClass("active");
       return;
     }
   }
 
+  //Play the notes that are selected
   function playSound(column) {
     var notesToPlay = [];
 
     $(column).children('.selected').each(function() {
+<<<<<<< HEAD
       var addNote = $(this).find('span').html();
       notesToPlay.push(addNote); 
       console.log(notesToPlay);
+=======
+      var addNote = $(this).attr('note');
+      notesToPlay.push(addNote);
+      console.log("The notes to play are: "+notesToPlay);
+>>>>>>> Audio-customizations
       });
-      for (var i = 0; i < notesToPlay.length; i++) {
-        console.log(notesToPlay[i]);
-        var soundToPlay = eval('sound' + notesToPlay[i]);
+      for (var iPlay = 0; iPlay < notesToPlay.length; iPlay++) {
+        //console.log(notesToPlay[iPlay]);
+        var soundToPlay = eval("settings.soundPack[" + (notesToPlay[iPlay]-1) + "]");
         soundToPlay.play();
       }
       notesToPlay = [];
