a396b0add102241f2cdf27ed4ac947d4c72a45aa
bsantero <brian.santero@gmail.com>
2014-11-08 14:32:24 -0600

    Merge development into gh-pages branch

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,24 @@
 $(function() {
   var settings = {
-    soundPack:[],
-    fadeInVal:100,
-    fadeOutVal:null,
-    loopStatus:null,
-    currentCol:1,
-    colInterval:120,
+    soundPack: [],
+    fadeInVal: 0,
+    fadeOutVal: null,
+    loopStatus: null,
+    defVolume: .5,
+    downbeatVol: 1,
+    currentCol: 1,
+    colInterval: 120,
     measures: 1,
-    beats:4,
-    subdivision:4,
+    beats: 4,
+    subdivision: 4,
     defaulCols: 16,
-    totalCols: 16,
-    selectedBlocks: []
+    totalCol: 16,
+    selectedBoxes: {}
   };
 
 //Setup
-  buildGrid();
   createSoundPack();
+  buildGrid();
 
   //Renders Tempo knob
   $(".knob").knob({
@@ -29,9 +31,7 @@ $(function() {
 
 //Event Handlers
   //Toggles selected class
-  $('body').on('click', '.box', function() {
-    $(this).toggleClass("selected");
-  });
+  $('body').on('click', '.box', boxSelected);
 
   //Event listeners for toggling Start/Stop
     //Click button
@@ -52,23 +52,26 @@ $(function() {
   //Reset button
   $(".clear").click(function() {
     $(".box").removeClass("selected");
+    for (var i in settings.selectedBoxes) {
+      settings.selectedBoxes[i] = [];
+    }
   });
     
   //Measures/Beats/Subdivision listeners
   $('.measures-container input').on('change', function() {
     settings.measures = $('input[name="measures"]:checked', '.measures-container').val(); 
-    resetGrid();
+    buildGrid();
   });
 
   $('.beats-container input').on('change', function() {
     settings.beats = $('input[name="beats"]:checked', '.beats-container').val(); 
-    resetGrid();
+    buildGrid();
   });
 
   $('.subdivision-container input').on('change', function() {
     settings.subdivision = $('input[name="subdivision"]:checked', '.subdivision-container').val(); 
-    console.log("Subdivision is now: "+settings.subdivision);
-    resetGrid();
+    //console.log("Subdivision is now: "+settings.subdivision);
+    buildGrid();
   });
 
   //Start button <-> Stop Button 
@@ -85,12 +88,26 @@ $(function() {
   }
 
 //Functions
+
+  //Dynamically create sound file references
+  function createSoundPack() {
+    for(var soundIndex = 1; soundIndex<=16; soundIndex++) {
+      settings.soundPack[soundIndex-1] = new Howl({
+        urls: ['media/'+soundIndex+'.ogg', 'media/'+soundIndex+'.mp3'],
+        fadeIn: settings.fadeInVal,
+        fadeOut: settings.fadeOutVal,
+        volume: settings.defVolume,
+      });
+    }
+  }
+
   //Dynamically build HTML grid
   function buildGrid() {
     var container = $(".grid");
-    settings.totalCols = settings.measures * settings.beats * settings.subdivision;
+    $(".grid").html("");
+    settings.totalCol = settings.measures * settings.beats * settings.subdivision;
     //Loop through each row.
-    for (var colIndex = 1; colIndex <= settings.totalCols; colIndex++) {
+    for (var colIndex = 1; colIndex <= settings.totalCol; colIndex++) {
       //Create the skeleton for the row.
       var col = $("<div>", {
           "class": "col col-" + colIndex,
@@ -101,69 +118,24 @@ $(function() {
         //Create skeleton for the column.
         var row = $("<div>", {
             "class": "box row-"+rowIndex,
-            "id": "col"+colIndex+"row"+rowIndex,
-            "note": rowIndex
+            "data-col": colIndex,
+            "data-row": rowIndex,
         });
         //Append to the row div.
         col.append(row);
       }
       //Finally append this row to the container.
       container.append(col);
-    }
-    findDownbeat();
-  }
 
-  //Dynamically create sound file references
-  function createSoundPack() {
-    for(var soundIndex = 1; soundIndex<=16; soundIndex++) {
-      settings.soundPack[soundIndex-1] = new Howl({
-        urls: ['media/'+soundIndex+'.ogg', 'media/'+soundIndex+'.mp3'],
-        fadeIn: settings.fadeInVal,
-        fadeOut: settings.fadeOutVal
-      });
-    }
-  }
-
-  function resetGrid() {
-    var container = $(".grid");
-    var newTotalCols = settings.measures * settings.beats * settings.subdivision;
-
-    if (newTotalCols !== settings.totalCols) {
-      if (newTotalCols > settings.totalCols) {
-        for (var currentCol = settings.totalCols + 1; currentCol <= newTotalCols; currentCol++) {
-          console.log(settings.totalCols+" and "+currentCol);
-          //Create the skeleton for the row.
-          var col = $("<div>", {
-              "class": "col col-"+ currentCol,
-              "id": "col-"+ currentCol
-          });
-          //Loop through each column
-          for (var rowIndex = 1; rowIndex <= 16; rowIndex++) {
-            //Create skeleton for the column.
-            var row = $("<div>", {
-                "class": "box row-"+rowIndex,
-                "id": "col"+currentCol+"row"+rowIndex,
-                "note": rowIndex
-            });
-            //Append to the row div.
-            col.append(row);
-          }
-            //Finally append this row to the container.
-            container.append(col);
-        }
-      } 
-      else if (newTotalCols < settings.totalCols) {
-        console.log("delete "+ (settings.totalCols - newTotalCols) +" columns");
-        for (var currentCol = settings.totalCols; currentCol > newTotalCols; currentCol--) {
-          console.log(currentCol);
-          $(".grid").children().last().remove();
-        }
+      if ((settings.selectedBoxes["col"+colIndex] != undefined) || (settings.selectedBoxes["col"+colIndex] != null)){
+        console.log(settings.selectedBoxes["col"+colIndex]);
+        populateColNotes(colIndex, settings.selectedBoxes["col"+colIndex]);
+      } else {
+        settings.selectedBoxes["col"+colIndex] = [];
       }
     }
-    $(".box").removeClass("active");
-    settings.totalCols = newTotalCols;
-    settings.currentCol = 1;
     findDownbeat();
+    console.log(settings.selectedBoxes);
   }
 
   function findDownbeat() {
@@ -171,45 +143,54 @@ $(function() {
     var measureMarkers = settings.beats * settings.subdivision;
 
     $(markedColumns).children().removeClass('downbeat downbeat-measure');
-    for (i = 0; i < markedColumns.length; i++ ) {
+    for (var i = 0; i < markedColumns.length; i++ ) {
       if (i % settings.subdivision === 0) {
         $(markedColumns[i]).children().addClass('downbeat');
-        console.log(i + "downbeats");
+        //console.log(i + "downbeats");
       }
     }
-    for (j = 0; j < markedColumns.length; j++ ) {
+    for (var j = 0; j < markedColumns.length; j++ ) {
       if (j % measureMarkers === 0) {
-        console.log(j + "measure beats");
+        //console.log(j + "measure beats");
         $(markedColumns[j]).children().addClass('downbeat-measure');
       }
     }
   }
 
-  //Set tempo
-  function setTempo(dataName, dataVal) {
-    var milliseconds = Math.round((((60/dataVal)*1000)*100000)/100000)/settings.subdivision;
-        //console.log("Sixteenth is precisely "+sixteenth+" milliseconds");
-        settings[dataName] = milliseconds;
+  function populateColNotes(col, array) {
+    for (var i = 0; i < array.length; i++) {
+      console.log("i is: "+i);
+      console.log("Col is: "+col);
+      console.log("Row is: "+array[i]);
+      var selector = $(".col-"+col).find(".row-"+array[i]).addClass("selected");
+      console.log(selector);
+    }
   }
+
 
   //Loop through every column
   function gridLoop() {
     if (settings.loopStatus) {
-      settings.totalCols = settings.measures * settings.beats * settings.subdivision;
+      settings.totalCol = settings.measures * settings.beats * settings.subdivision;
       setTimeout( function() {
         $(".col-" + settings.currentCol).children().toggleClass("active");
-        if (settings.currentCol == 1) {
-          $(".col-"+settings.totalCols).children().removeClass("active");
+        if (settings.currentCol === 1) {
+          $(".col-" + settings.totalCol).children().removeClass("active");
         }
         else {
           $(".col-"+(settings.currentCol - 1)).children().removeClass("active");
         }
-        playSound(".col-" + settings.currentCol);
+        //
+        if ( (settings.currentCol === 1) || (settings.currentCol % settings.subdivision === 0) ) {
+          playSound(settings.downbeatVol);
+        } else {
+          playSound(settings.defVolume);
+        }
         settings.currentCol++;
-        if (settings.currentCol <= settings.totalCols){
+        if (settings.currentCol <= settings.totalCol){
           gridLoop();        
         }
-        else if (settings.currentCol == settings.totalCols + 1) {
+        else if (settings.currentCol == settings.totalCol + 1) {
           settings.currentCol = 1;
           gridLoop();
         }
@@ -223,19 +204,35 @@ $(function() {
   }
 
   //Play the notes that are selected
-  function playSound(column) {
-    var notesToPlay = [];
-
-    $(column).children('.selected').each(function() {
-      var addNote = $(this).attr('note');
-      notesToPlay.push(addNote);
-      //console.log("The notes to play are: "+notesToPlay);
-      });
-      for (var iPlay = 0; iPlay < notesToPlay.length; iPlay++) {
-        var soundToPlay = eval("settings.soundPack[" + (notesToPlay[iPlay]-1) + "]");
-        soundToPlay.play();
-      }
-      notesToPlay = [];
+  function playSound(noteVol) {
+    var notesToPlay = settings.selectedBoxes["col" + settings.currentCol];
+    for (var i = 0; i < notesToPlay.length; i++) {
+      var soundToPlay = eval("settings.soundPack[" + (notesToPlay[i]-1) + "]");
+      soundToPlay.play();
+      soundToPlay.volume(noteVol);
     }
+    notesToPlay = [];
+  }
+
+
+  //Set tempo
+  function setTempo(dataName, dataVal) {
+    var milliseconds = Math.round((((60/dataVal)*1000)*100000)/100000)/settings.subdivision;
+        //console.log("Sixteenth is precisely "+sixteenth+" milliseconds");
+        settings[dataName] = milliseconds;
+  }
+
+  //When Box is Selected
+  function boxSelected() {
+    var col = $(this).attr('data-col');
+    var row = $(this).attr('data-row');
+    var array = settings.selectedBoxes["col"+col];
+    if (array.indexOf(row) !== -1) {
+      array.splice(array.indexOf(row), 1);
+    } else {
+      array.push(row);
+    }
+    $(this).toggleClass("selected");
+  }
 
 });
